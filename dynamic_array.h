b4661f191d48553c83aa5b884d07d0cdbc1d0df5
kucer <kucerovzahar870@gmail.com>
2026-04-02 12:40:19 +0300

    Fix dynamic array: C compatibility, bugfix, add capacity & insert

--- dynamic_array.h
+++ dynamic_array.h
@@ -18,7 +18,7 @@ typedef struct {
 #define arr_push(arr, x)                                                                \
     do {                                                                                \
         if (arr == NULL) {                                                              \
-            Header *header = malloc(sizeof(int) * 1 + sizeof(Header));                  \
+            Header *header = malloc(sizeof(*(arr)) * 1 + sizeof(Header));               \
                                                                                         \
             header->size = 0;                                                           \
             header->capacity = 1;                                                       \
@@ -45,7 +45,7 @@ typedef struct {
 
 #define arr_pop(arr, out)                                                      \
     do {                                                                       \
-        if (arr == nullptr) {                                                  \
+        if (arr == NULL) {                                                     \
             fprintf(stderr, "arr_pop: array is NULL\n");                       \
             break;                                                             \
         }                                                                      \
@@ -62,11 +62,45 @@ typedef struct {
 #define arr_len(arr) ((Header *) (arr) - 1)->size
 
 
+#define arr_capacity(arr) ((Header *) (arr) - 1)->capacity
+
+
 #define arr_free(arr) free((Header*)(arr) - 1)
 
 
-#define arr_foreach(arr, var, block)                            \
-    for (size_t i_ = 0; i_ < arr_len(arr); ++i_) {              \
-        typeof(*(arr)) var = (arr)[i_];                         \
-        block;                                                  \
-    }
+#define arr_insert(arr, idx, val)                                                 \
+    do {                                                                          \
+        size_t len = arr_len(arr);                                                \
+        if ((idx) > len) {                                                        \
+            fprintf(stderr, "arr_insert: index out of range\n");                  \
+            break;                                                                \
+        }                                                                         \
+        if (len + 1 > arr_capacity(arr)) {                                        \
+            Header *header = (Header*)(arr) - 1;                                  \
+            size_t new_cap = header->capacity ? header->capacity * 2 : 1;         \
+            header = realloc(header, sizeof(*(arr)) * new_cap + sizeof(Header));  \
+            if (header == NULL) {                                                 \
+                fprintf(stderr, "arr_insert: realloc failed\n");                  \
+                break;                                                            \
+            }                                                                     \
+            header->capacity = new_cap;                                           \
+            arr = (void*)(header + 1);                                            \
+        }                                                                         \
+        Header *header = (Header*)(arr) - 1;                                      \
+        for (size_t i = header->size; i > (idx); i--)                             \
+            (arr)[i] = (arr)[i-1];                                                \
+        (arr)[idx] = (val);                                                       \
+        header->size++;                                                           \
+    } while(0)
+
+
+#if __STDC_VERSION__ >= 202311L
+    #define arr_foreach(arr, var, block)                            \
+        for (size_t i_ = 0; i_ < arr_len(arr); ++i_) {              \
+            typeof(*(arr)) var = (arr)[i_];                         \
+            block;                                                  \
+        }
+#else
+    #define arr_foreach(arr, idx_var) \
+        for (size_t idx_var = 0; idx_var < arr_len(arr); ++idx_var)
+#endif
