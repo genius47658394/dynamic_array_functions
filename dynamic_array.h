ac2c78eac9cab823f1ff4e80ab3a73f250c5d1d6
genius47658394 <kucerovzahar870@gmail.com>
2026-04-19 11:38:14 +0300

    Update dynamic_array.h

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,106 +1,110 @@
 //
-// Created by kucer on 02.04.2026.
+// Created by kucer on 19.04.2026.
 //
 
 #pragma once
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-// to understand how it works, watch: https://www.youtube.com/watch?v=gtk3RZHwJUA
+#include <string.h>
 
 typedef struct {
-    size_t size;
+    size_t count;
     size_t capacity;
 } Header;
 
+#define ARR_INIT_CAPACITY 2
 
-#define arr_push(arr, x)                                                                \
-    do {                                                                                \
-        if (arr == NULL) {                                                              \
-            Header *header = malloc(sizeof(*(arr)) * 1 + sizeof(Header));               \
-                                                                                        \
-            header->size = 0;                                                           \
-            header->capacity = 1;                                                       \
-                                                                                        \
-            arr = (void*) (header + 1);                                                 \
-        }                                                                               \
-                                                                                        \
-        Header *header = (Header *) (arr) - 1;                                          \
-        if (header->size >= header->capacity) {                                         \
-            header->capacity *= 2;                                                      \
-                                                                                        \
-            header = realloc(header, sizeof(*arr) * header->capacity + sizeof(Header)); \
-                                                                                        \
-            if (header == NULL) {                                                       \
-                printf("Error: can`t realloc");                                         \
-                break;                                                                  \
-            }                                                                           \
-                                                                                        \
-            arr = (void*) (header + 1);                                                 \
-        }                                                                               \
-        (arr)[header->size++] = x;                                                      \
-    } while(0)
+#define arr_header(arr)       ((Header*)(arr) - 1)
+#define arr_len(arr)          (arr_header(arr)->count)
+#define arr_capacity(arr)     (arr_header(arr)->capacity)
 
+#define arr_free(arr)         free(arr_header(arr))
 
-#define arr_pop(arr, out)                                                      \
-    do {                                                                       \
-        if (arr == NULL) {                                                     \
-            fprintf(stderr, "arr_pop: array is NULL\n");                       \
-            break;                                                             \
-        }                                                                      \
-        Header *header = (Header*)(arr) - 1;                                   \
-        if (header->size == 0) {                                               \
-            fprintf(stderr, "arr_pop: array is empty\n");                      \
-            break;                                                             \
-        }                                                                      \
-        header->size--;                                                        \
-        (out) = (arr)[header->size];                                           \
-    } while(0)
+#define arr_free_and_null(arr) do { \
+    arr_free(arr); \
+    (arr) = NULL; \
+} while(0)
 
+#define arr_reserve(arr, new_cap) do { \
+    if ((arr) == NULL) { \
+        Header *h_ = malloc(sizeof(*(arr)) * (new_cap) + sizeof(Header)); \
+        assert(h_ != NULL); \
+        h_->count = 0; \
+        h_->capacity = (new_cap); \
+        (arr) = (void*)(h_ + 1); \
+    } else { \
+        Header *h_ = arr_header(arr); \
+        if (h_->capacity < (new_cap)) { \
+            h_ = realloc(h_, sizeof(*(arr)) * (new_cap) + sizeof(Header)); \
+            assert(h_ != NULL); \
+            h_->capacity = (new_cap); \
+            (arr) = (void*)(h_ + 1); \
+        } \
+    } \
+} while(0)
 
-#define arr_len(arr) ((Header *) (arr) - 1)->size
+#define arr_push(arr, x) do { \
+    if ((arr) == NULL) { \
+        arr_reserve(arr, ARR_INIT_CAPACITY); \
+    } else { \
+        Header *h_ = arr_header(arr); \
+        if (h_->count >= h_->capacity) { \
+            arr_reserve(arr, h_->capacity * 2); \
+        } \
+    } \
+    Header *h_ = arr_header(arr); \
+    (arr)[h_->count++] = (x); \
+} while(0)
 
+#define arr_pop(arr) ( \
+    assert((arr) != NULL && arr_len(arr) > 0), \
+    (arr)[--arr_header(arr)->count] \
+)
 
-#define arr_capacity(arr) ((Header *) (arr) - 1)->capacity
+#define arr_insert(arr, index, x) do { \
+    size_t len_ = (arr) ? arr_len(arr) : 0; \
+    assert((index) <= len_); \
+    if ((arr) == NULL || len_ >= arr_capacity(arr)) { \
+        arr_reserve(arr, (arr) ? arr_capacity(arr)*2 : ARR_INIT_CAPACITY); \
+    } \
+    Header *h_ = arr_header(arr); \
+    memmove(&(arr)[(index)+1], &(arr)[index], (len_ - (index)) * sizeof(*(arr))); \
+    (arr)[index] = (x); \
+    h_->count++; \
+} while(0)
 
+#define arr_erase(arr, index) do { \
+    size_t len_ = arr_len(arr); \
+    assert((index) < len_); \
+    Header *h_ = arr_header(arr); \
+    memmove(&(arr)[index], &(arr)[(index)+1], (len_ - (index) - 1) * sizeof(*(arr))); \
+    h_->count--; \
+} while(0)
 
-#define arr_free(arr) free((Header*)(arr) - 1)
+#define arr_shrink_to_fit(arr) do { \
+    if ((arr) == NULL) break; \
+    Header *h_ = arr_header(arr); \
+    if (h_->count == 0) { \
+        free(h_); \
+        (arr) = NULL; \
+    } else if (h_->count < h_->capacity) { \
+        h_ = realloc(h_, sizeof(*(arr)) * h_->count + sizeof(Header)); \
+        assert(h_ != NULL); \
+        h_->capacity = h_->count; \
+        (arr) = (void*)(h_ + 1); \
+    } \
+} while(0)
 
+#define arr_foreach(arr, elem) \
+    for (size_t _i = 0; _i < arr_len(arr) && ((elem) = (arr)[_i], 1); ++_i)
 
-#define arr_insert(arr, idx, val)                                                 \
-    do {                                                                          \
-        size_t len = arr_len(arr);                                                \
-        if ((idx) > len) {                                                        \
-            fprintf(stderr, "arr_insert: index out of range\n");                  \
-            break;                                                                \
-        }                                                                         \
-        if (len + 1 > arr_capacity(arr)) {                                        \
-            Header *header = (Header*)(arr) - 1;                                  \
-            size_t new_cap = header->capacity ? header->capacity * 2 : 1;         \
-            header = realloc(header, sizeof(*(arr)) * new_cap + sizeof(Header));  \
-            if (header == NULL) {                                                 \
-                fprintf(stderr, "arr_insert: realloc failed\n");                  \
-                break;                                                            \
-            }                                                                     \
-            header->capacity = new_cap;                                           \
-            arr = (void*)(header + 1);                                            \
-        }                                                                         \
-        Header *header = (Header*)(arr) - 1;                                      \
-        for (size_t i = header->size; i > (idx); i--)                             \
-            (arr)[i] = (arr)[i-1];                                                \
-        (arr)[idx] = (val);                                                       \
-        header->size++;                                                           \
-    } while(0)
+#define arr_sort(arr, cmp) do { \
+    if ((arr) != NULL) { \
+        qsort((arr), arr_len(arr), sizeof(*(arr)), (cmp)); \
+    } \
+} while(0)
 
-
-#if __STDC_VERSION__ >= 202311L
-    #define arr_foreach(arr, var, block)                            \
-        for (size_t i_ = 0; i_ < arr_len(arr); ++i_) {              \
-            typeof(*(arr)) var = (arr)[i_];                         \
-            block;                                                  \
-        }
-#else
-    #define arr_foreach(arr, idx_var) \
-        for (size_t idx_var = 0; idx_var < arr_len(arr); ++idx_var)
-#endif
+#define arr_bsearch(arr, key, cmp) \
+    bsearch((key), (arr), arr_len(arr), sizeof(*(arr)), (cmp))
